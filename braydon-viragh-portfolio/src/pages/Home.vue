c871650941538dcad4fbaaf4ca63cc317f9fe9fe
Braydon Viragh <bradviragh@gmail.com>
2024-09-04 20:34:26 -0400

    proper resume and content fixing

--- braydon-viragh-portfolio/src/pages/Home.vue
+++ braydon-viragh-portfolio/src/pages/Home.vue
@@ -5,7 +5,8 @@
         <div class="text-h2 q-mb-md">Braydon Viragh</div>
         <div class="text-h5 q-mb-lg">Full Stack Developer <span class="text-primary">Frontend UI/UX Focused</span></div>
         <p class="text-body1 q-mb-lg">
-            I thrive on taking on complex challenges, learning new skills, and building great products.
+          Self-taught, self-driven, self-motivated.
+          Love taking on challenges, learning new skills, and building great products.
         </p>
         <div class="flex justify-center q-gutter-md">
           <q-btn
